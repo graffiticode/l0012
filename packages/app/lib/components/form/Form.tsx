c3aba742a45cf4a559fae7a6363cea2c91e4ba73
Jeff Dyer <jeffdyer@acm.org>
2025-02-26 16:44:16 -0800

    Don't delete theme and schema

--- packages/app/lib/components/form/Form.tsx
+++ packages/app/lib/components/form/Form.tsx
@@ -1,15 +1,5 @@
 import "../../index.css";
-import { useState, useEffect } from "react";
-
-import { ThemeToggle } from "./ThemeToggle";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
 function renderJSON(data) {
-  delete data.schema;
-  delete data.theme;
   return (
     <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
   );
@@ -21,24 +11,5 @@ function render({ state }) {
 }
 
 export const Form = ({ state }) => {
-  const [ theme, setTheme ] = useState(state.data.theme);
-  useEffect(() => {
-    state.apply({
-      type: "update",
-      args: {
-        theme,
-      },
-    });
-  }, [theme]);
-  return (
-    <div
-      className={classNames(
-        theme === "dark" && "bg-zinc-900 text-white" || "bg-white text-zinc-900",
-        "rounded-md font-mono flex flex-col gap-4 p-4"
-      )}
-    >
-      {theme !== undefined && <ThemeToggle theme={theme} setTheme={setTheme} />}
-      {render({state})}
-    </div>
-  );
+  return render({state});
 }
